904c5f4bb256eeacc545850a15de44802decdd22
Steve Mbiele <stevembiele@gmail.com>
2021-11-21 21:30:26 -0500

    chore: test fix

--- test/NuxtLogo.spec.js
+++ test/NuxtLogo.spec.js
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils'
-import TwitchAuth from '@/components/TwitchAuth.vue'
+import TwitchAuth from '@/components/TwitchAuth/TwitchAuth.vue'
 
 describe('TwitchAuth', () => {
   test('is a Vue instance', () => {
